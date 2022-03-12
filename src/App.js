7af636d2bb8f310686acc5e4fc3379f854fe38a0
carch921 <walid.sabri@esprit.tn>
2022-03-12 13:30:52 +0100

    first comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
+import imageInSrc from './imageInSrc.jpg'
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}} >
+      <h1 className="title red">Walid Sabri</h1>
+
+<br/>
+
+<img src="/imageInPublic.jpg" alt='imageInPublic'/>
+<br/>
+<img src={imageInSrc} alt='imageInSrc'/>
+</div>
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
     </div>
   );
 }
